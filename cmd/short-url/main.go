4d3993a4d2c3034a17e86cfe34273e29b6f7e2d5
Alex322322 <alex9999091@gmail.com>
2025-11-21 14:13:21 +0300

    add logger

--- cmd/short-url/main.go
+++ cmd/short-url/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+
+	"log/slog"
 
 	"github.com/Alex322322/short-url/internal/config"
 	"github.com/joho/godotenv"
+)
+
+const (
+	envLocal       = "local"
+	envProduction  = "prod"
+	envDevelopment = "dev"
 )
 
 func main() {
@@ -16,10 +24,36 @@ func main() {
 
 	// TODO init config - cleanenv
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	//fmt.Println(cfg)
 
 	// TODO init logger - slog log/slog
+	logger := setupLogger(cfg.Env)
+	logger.Info("Logger short-url initialized", slog.String("env", cfg.Env))
+	logger.Debug("Debug is turned on")
+
 	// TODO init storage - sqlite/postgres
 	// TODO init router - chi
 	// TODO run server - net/http
+}
+
+// setup logger based on environment
+func setupLogger(env string) *slog.Logger {
+	
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDevelopment:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProduction:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return logger
 }
