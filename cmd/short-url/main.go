188b4a4f40b92c4f99dfd6d5994d3fd6a4c4eb57
Alex322322 <alex9999091@gmail.com>
2025-11-24 20:53:49 +0300

    add mocks amd test for delete handler

--- cmd/short-url/main.go
+++ cmd/short-url/main.go
@@ -9,7 +9,8 @@ import (
 	"log/slog"
 
 	"github.com/Alex322322/short-url/internal/config"
-	"github.com/Alex322322/short-url/internal/http/server/handlers/redirect"
+	"github.com/Alex322322/short-url/internal/http/server/handlers/url/delete"
+	"github.com/Alex322322/short-url/internal/http/server/handlers/url/redirect"
 	"github.com/Alex322322/short-url/internal/http/server/handlers/url/save"
 	mwLogger "github.com/Alex322322/short-url/internal/http/server/middleware/logger"
 	"github.com/Alex322322/short-url/internal/storage/sqlite"
@@ -68,6 +69,7 @@ func main() {
 
 	router.Post("/url", save.New(logger, storage))
 	router.Get("/{alias}", redirect.New(logger, storage))
+	router.Delete("/{alias}", delete.New(logger, storage))
 
 	// TODO run server - net/http
 	logger.Info("Starting HTTP server", slog.String("address", cfg.HTTPServer.Address))
