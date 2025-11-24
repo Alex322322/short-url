7496420006a6d0d9e2d1466081ac670e5c137fc0
Alex322322 <alex9999091@gmail.com>
2025-11-25 02:44:02 +0300

    add redirect unit test

--- internal/http/server/handlers/url/redirect/redirect.go
+++ internal/http/server/handlers/url/redirect/redirect.go
@@ -6,7 +6,7 @@ import (
 
 	resp "github.com/Alex322322/short-url/internal/lib/api/response"
 	"github.com/Alex322322/short-url/internal/storage"
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 )
--- internal/http/server/handlers/url/redirect/redirect_test.go
+++ internal/http/server/handlers/url/redirect/redirect_test.go
@@ -1,111 +1,56 @@
 package redirect_test
 
 import (
-	"encoding/json"
-	"errors"
-	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/Alex322322/short-url/internal/http/server/handlers/url/redirect"
+	"github.com/Alex322322/short-url/internal/lib/api"
 	"github.com/Alex322322/short-url/internal/lib/logger/discardslog"
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRedirectHandler(t *testing.T) {
-	fixedTime := time.Now().Truncate(time.Second) // обрезаем до секунд для стабильности
+func TestSaveHandler(t *testing.T) {
 	cases := []struct {
-		name         string
-		alias        string
-		url          string
-		timestamp    time.Time
-		respError    string
-		mockError    error
-		expectedCode int
-		expectedURL  string
+		name      string
+		alias     string
+		url       string
+		respError string
+		mockError error
 	}{
 		{
-			name:         "Success redirect",
-			alias:        "test_alias",
-			url:          "https://google.com",
-			timestamp:    fixedTime,
-			expectedCode: http.StatusFound,
-			expectedURL:  "https://google.com",
-		},
-		{
-			name:         "Empty alias",
-			alias:        "",
-			url:          "https://google.com",
-			expectedCode: http.StatusBadRequest,
-			timestamp:    fixedTime,
-		},
-		{
-			name:         "Empty URL",
-			url:          "",
-			alias:        "some_alias",
-			expectedCode: http.StatusBadRequest,
-			timestamp:    fixedTime,
-		},
-		{
-			name:         "Invalid URL",
-			url:          "some invalid URL",
-			alias:        "some_alias",
-			respError:    "URL is not a valid URL",
-			expectedCode: http.StatusBadRequest,
-			timestamp:    fixedTime,
-		},
-		{
-			name:         "URL Not Found",
-			alias:        "non_existent_alias",
-			respError:    "URL not found",
-			mockError:    errors.New("URL not found"),
-			timestamp:    fixedTime,
-			expectedCode: http.StatusNotFound,
-		},
-		{
-			name:         "GetURL Error",
-			alias:        "test_alias",
-			respError:    "internal server error",
-			mockError:    errors.New("unexpected error"),
-			timestamp:    fixedTime,
-			expectedCode: http.StatusInternalServerError,
+			name:  "Success",
+			alias: "test_alias",
+			url:   "https://www.google.com/",
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
+			//t.Parallel()
 
 			urlGetterMock := redirect.NewMockURLGetter(t)
 
 			if tc.respError == "" || tc.mockError != nil {
 				urlGetterMock.On("GetURL", tc.alias).
-					Return(tc.url, tc.mockError).
-					Once()
+					Return(tc.url, tc.mockError).Once()
 			}
 
 			handler := redirect.New(discardslog.NewDiscardLogger(), urlGetterMock)
 
-			req, err := http.NewRequest(http.MethodGet, "/"+tc.alias, nil)
+			r := chi.NewRouter()
+			r.Get("/{alias}", handler)
+
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+
+			redirectedToURL, err := api.GetRedirect(ts.URL + "/" + tc.alias)
 			require.NoError(t, err)
 
-			rr := httptest.NewRecorder()
-			handler.ServeHTTP(rr, req)
-
-			require.Equal(t, rr.Code, tc.expectedCode)
-
-			if tc.expectedCode == http.StatusFound {
-				require.Equal(t, tc.expectedURL, rr.Header().Get("Location"))
-			}
-			if tc.respError != "" {
-				var resp struct {
-					Error string `json:"error"`
-				}
-				body := rr.Body.String()
-				require.NoError(t, json.Unmarshal([]byte(body), &resp))
-				require.Equal(t, tc.respError, resp.Error)
-			}
+			// check the final URL after redirection
+			assert.Equal(t, tc.url, redirectedToURL)
 		})
 	}
 }
